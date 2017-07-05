40dcf2ad183d1f241e904184c70c0c3520b8e87a
Christian Yves Dubuche <dubuche@gmail.com>
2017-07-05 11:30:18 -0400

    Add project files.

---
+++ FSDPortfolio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FSDPortfolio.Startup))]
+namespace FSDPortfolio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
